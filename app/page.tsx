d336d73690ee18ee89daa918f5f9de0833360a12
nanafromjeju <nanafromjeju@gmail.com>
2024-10-12 19:26:46 +0900

    help: 폰트 적용 (#6)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 const HomePage = () => (
   <main className="p-4">
-    <h1 className="text-2xl font-bold mb-4">Home</h1>
+    <h1 className="text-2xl font-neodgm">Home - 네오둥근 이거랑 다른가 Neodgm</h1>
+    <h1 className="text-2xl font-galmuri">Home - 갈무리 이거랑 다른가 Galmuri</h1>
   </main>
 )
 
