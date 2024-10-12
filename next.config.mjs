ae4f9b75779a9d76c9afc8c56555614cf0fd53ac
James <dawn147@snu.ac.kr>
2024-10-12 10:42:38 +0900

    settings: 빠진 의존성 추가 (#5)

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,19 @@ const nextConfig = {
         ]
       }
     ]
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
