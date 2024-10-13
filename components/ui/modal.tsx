917c6efa091dca44eec869565b3290fba3028cf7
nanafromjeju <nanafromjeju@gmail.com>
2024-10-13 23:12:27 +0900

    feat: 모달 공통컴포넌트 작업 (#17)

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,17 +1,53 @@
-import CloseIcon from '@mui/icons-material/Close'
+'use client'
 
-const Modal = () => (
-  <>
-    <div>
-      <h1>게임방법</h1>
-      <CloseIcon className="cursor-pointer" />
-      <p>각 플레이어는 게임 시작 시 1,000냥코인을 지급받습니다.</p>
-      <p>생선 가격은 첫날 100냥코인으로 시작해, 매일 1~300냥코인 사이에서 변동됩니다.</p>
-      <p>20초 안에 생선을 사고 팔아야 하며, '사기' 또는 '팔기' 버튼을 눌러 거래할 수 있습니다.</p>
-      <p>최종적으로 가장 많은 냥코인을 모은 플레이어가 우승합니다.</p>
-      <p>최적의 타이밍에 거래해서 우승을 차지하세요!</p>
+import { useState } from 'react'
+import CloseIcon from '@mui/icons-material/Close'
+import Button from '../ui/button'
+
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+interface ModalProps {
+  title?: string
+  content?: string
+  buttonText?: string
+  showButton?: boolean
+}
+
+const Modal = ({ title = '', content = '', buttonText = '', showButton = false }: ModalProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(true)
+
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+  }
+
+  if (!isModalOpen) return null
+
+  return (
+    <div
+      className={twMerge(
+        clsx('fixed inset-0 flex items-center justify-center bg-black bg-opacity-50')
+      )}
+      onClick={handleModalClose}
+    >
+      <div
+        className={twMerge(clsx('w-[300px] overflow-hidden rounded-lg shadow-md'))}
+        onClick={(event) => event.stopPropagation()}
+      >
+        <div
+          className={twMerge(clsx('flex h-[40px] items-center justify-between bg-primary px-4'))}
+        >
+          <h2 className="w-full text-center text-lg text-white">{title}</h2>
+          <CloseIcon onClick={handleModalClose} className="cursor-pointer text-white" />
+        </div>
+
+        <div className="bg-white p-4">
+          <p className="mt-2 text-sm">{content}</p>
+          {showButton && <Button>{buttonText}</Button>}
+        </div>
+      </div>
     </div>
-  </>
-)
+  )
+}
 
 export default Modal
