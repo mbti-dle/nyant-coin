ce7686eee2fdb780ac9d9221bc2abb1512ea8eaa
James <dawn147@snu.ac.kr>
2024-10-14 14:57:47 +0900

    feat: 인풋 컴포넌트 파일명 변경 및 상속하도록 수정 (#14)

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -5,15 +5,14 @@ import { ComponentProps } from 'react'
 import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-interface CodeInputProps extends ComponentProps<'input'> {
+interface InputProps extends ComponentProps<'input'> {
   value: string
 }
 
-const CodeInput = ({ className, value, onChange, ...props }: CodeInputProps) => {
+const Input = ({ className, value, onChange, ...props }: InputProps) => {
   const inputClassName = twMerge(
     clsx(
-      'h-full w-full rounded-xl px-4 pr-16 text-lg outline-none',
-      'bg-white border-2 border-primary text-black placeholder-gray-200'
+      'h-full w-full rounded-xl px-4 pr-16 text-lg outline-none bg-white border-2 border-primary text-black placeholder-gray-200'
     ),
     className
   )
@@ -31,4 +30,4 @@ const CodeInput = ({ className, value, onChange, ...props }: CodeInputProps) => 
   )
 }
 
-export default CodeInput
+export default Input
