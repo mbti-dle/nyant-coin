de9bf1fe1c3f0ab5e2a2ae1057a340dd232c4e42
James <dawn147@snu.ac.kr>
2024-10-14 15:22:32 +0900

    feat: 불필요한 코드 제거 (#14)

--- components/ui/count-input.tsx
+++ components/ui/count-input.tsx
@@ -4,9 +4,7 @@ import { ComponentProps } from 'react'
 
 import Input from '@/components/ui/input'
 
-interface CountInputProps extends ComponentProps<typeof Input> {
-  maxLength?: number
-}
+interface CountInputProps extends ComponentProps<typeof Input> {}
 
 const CountInput = ({ maxLength = 10, value, onChange, ...props }: CountInputProps) => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,9 +16,14 @@ const CountInput = ({ maxLength = 10, value, onChange, ...props }: CountInputPro
 
   return (
     <div className="relative h-[54px] w-[300px]">
-      <Input value={value} onChange={handleInputChange} maxLength={maxLength} {...props} />
+      <Input 
+        value={value} 
+        onChange={handleInputChange} 
+        maxLength={maxLength} 
+        {...props} 
+      />
       <span className="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 font-bold text-gray-200">
-        {value.length}/{maxLength}
+        {value?.length || 0}/{maxLength}
       </span>
     </div>
   )
