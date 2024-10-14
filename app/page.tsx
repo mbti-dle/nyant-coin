f505e6363ad33a3c04e97039907a0d31cb4022ac
nanafromjeju <nanafromjeju@gmail.com>
2024-10-14 20:13:56 +0900

    feature: 모달 포탈로 변경 (#17)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Modal from '../components/ui/modal'
-
-const HomePage = () => (
-  <main className="p-4">
-    <Modal></Modal>
-  </main>
-)
+const HomePage = () => <main className="p-4"></main>
 
 export default HomePage
