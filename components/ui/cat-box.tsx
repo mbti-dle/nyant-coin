d2a39509c90a3f33809eee27f0169c1788b05c20
James <dawn147@snu.ac.kr>
2024-10-14 21:00:26 +0900

    feat: clsx를 사용하여 조건부 렌더링 코드 간소화 (#39)

--- components/ui/cat-box.tsx
+++ components/ui/cat-box.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps } from 'react'
 
+import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 interface CatBoxProps extends ComponentProps<'div'> {
@@ -17,31 +18,41 @@ const CatBox = ({
   className,
 }: CatBoxProps) => {
   const isMobile = type === 'mobile'
-  const containerSize = isMobile ? 'h-[106px] w-[106px]' : 'h-[135px] w-[135px]'
-  const imgSize = isMobile ? 'w-[60px] h-[60px]' : 'w-[76px] h-[76px]'
-  const crownSize = isMobile ? 'w-4 h-4' : 'w-5 h-5'
-  const imageMargin = isMobile ? '' : 'mt-2'
 
   return (
     <div
       className={twMerge(
-        `flex flex-col items-center rounded-xl bg-gray-50 bg-opacity-40 pt-3 ${containerSize} relative`,
-        className
+        clsx(
+          'relative flex flex-col items-center rounded-xl bg-gray-50 bg-opacity-40 pt-3',
+          {
+            'h-[106px] w-[106px]': isMobile,
+            'h-[135px] w-[135px]': !isMobile,
+          },
+          className
+        )
       )}
     >
       {isLeader && (
         <img
           src="/images/crown.png"
-          alt="Leader Crown"
-          className={`absolute right-2 top-1 ${crownSize}`}
+          alt="리더 왕관"
+          className={clsx('absolute right-2 top-1', {
+            'h-4 w-4': isMobile,
+            'h-5 w-5': !isMobile,
+          })}
         />
       )}
-      <div className={`flex-shrink-0 ${imgSize} ${imageMargin}`}>
+      <div
+        className={clsx('flex-shrink-0', {
+          'h-[60px] w-[60px]': isMobile,
+          'mt-2 h-[76px] w-[76px]': !isMobile,
+        })}
+      >
         {imageUrl && (
           <img
             src={imageUrl}
-            alt={nickName || 'avatar'}
-            className={`object-contain ${imgSize} ml-1`}
+            alt={nickName || '고양이 아바타'}
+            className="ml-1 h-full w-full object-contain"
           />
         )}
       </div>
