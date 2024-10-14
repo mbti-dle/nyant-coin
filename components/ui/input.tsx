72667b30656456ed482e333ce9f41912709da8ec
James <dawn147@snu.ac.kr>
2024-10-14 15:22:32 +0900

    feat: 불필요한 코드 제거 (#14)

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,26 +2,21 @@
 
 import { ComponentProps } from 'react'
 
-import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 interface InputProps extends ComponentProps<'input'> {
   value: string
 }
 
-const Input = ({ className, value, onChange, ...props }: InputProps) => {
-  const inputClassName = twMerge(
-    clsx(
-      'h-full w-full rounded-xl px-4 pr-16 text-lg outline-none bg-white border-2 border-primary text-black placeholder-gray-200'
-    ),
-    className
-  )
-
+const Input = ({ className, value, maxLength = 6, onChange, ...props }: InputProps) => {
   return (
     <div className="relative h-[54px] w-[300px]">
       <input
-        maxLength={10}
-        className={inputClassName}
+        maxLength={maxLength}
+        className={twMerge(
+    'h-full w-full rounded-xl px-4 pr-16 text-lg outline-none bg-white border-2 border-primary text-black placeholder-gray-200',
+    className
+  )}
         onChange={onChange}
         value={value}
         {...props}
