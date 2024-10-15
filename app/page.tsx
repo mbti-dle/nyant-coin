8923e7cc28f44513fdcf23b393b6022cbfc270a6
nanafromjeju <nanafromjeju@gmail.com>
2024-10-15 23:34:58 +0900

    feature: 메인페이지 시작 (#38)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-const HomePage = () => <main className="p-4"></main>
+import MainPage from '@/app/main'
+const HomePage = () => (
+  <main className="p-4">
+    <MainPage></MainPage>
+  </main>
+)
 
 export default HomePage
