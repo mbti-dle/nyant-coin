096cb9723164e608b12e3ee5bf934b65b0336a1e
Mark Kwon <redhero8830@gmail.com>
2024-10-15 22:38:30 +0900

    remove: 불필요한 `-fix` 폴더 삭제

---
+++ components/features/waiting-room/chatting.tsx
@@ -0,0 +1,10 @@
+const Chatting = () => {
+  return (
+    <div className="flex h-[194px] w-[358px] flex-col">
+      <div>container</div>
+      <div>input</div>
+    </div>
+  )
+}
+
+export default Chatting
