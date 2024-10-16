c97748aa15b4f88d8ce10c9bd3142c900a43fecf
deun <devdeun@gmail.com>
2024-10-16 22:29:13 +0900

    feat: 일수 선택 페이지 퍼블리싱 (#36)

--- app/setup/select-days/page.tsx
+++ app/setup/select-days/page.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
 
 import LinkButton from '@/components/ui/link-button'
+import RadioGroup, { RadioButton } from '@/components/ui/radio'
+import { DAY_OPTIONS } from '@/constants/game'
 
-const SelectDaysPage = ({ params }) => {
-  const { gameId = 'N09C14' } = params
+const SelectDaysPage = () => {
+  const gameId = 'N09C14'
+  const [selectedDay, setSelectedDay] = useState(DAY_OPTIONS[0])
+
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDay(event.target.value)
+  }
 
   return (
-    <>
-      <h2>일수선책</h2>
+    <main className="flex min-h-dvh flex-col items-center justify-center">
+      <h1 className="text-center text-xl">시장에서 거래할 일수를 정해주세요.</h1>
+      <RadioGroup className="my-20">
+        {DAY_OPTIONS.map((day) => (
+          <RadioButton
+            key={day}
+            name="select-days"
+            label={day}
+            isChecked={day === selectedDay}
+            onChangeInput={handleChangeInput}
+          />
+        ))}
+      </RadioGroup>
       <LinkButton href={`/setup/user-info/${gameId}`}>다음</LinkButton>
-    </>
+    </main>
   )
 }
 
