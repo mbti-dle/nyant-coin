101219db88d33aa91384b8a9d8ff72e2ca75cc30
Mark Kwon <redhero8830@gmail.com>
2024-10-16 22:09:42 +0900

    style: 사용하지 않는 import 코드 삭제 (#43)

--- app/waiting/[gameId]/page.tsx
+++ app/waiting/[gameId]/page.tsx
@@ -3,7 +3,6 @@
 import ChatContainer from '@/components/features/waiting/chat-container'
 import GameIdCopyButton from '@/components/features/waiting/game-id-copy-button'
 import PlayerGrid from '@/components/features/waiting/player-grid'
-import Button from '@/components/ui/button'
 import LinkButton from '@/components/ui/link-button'
 
 const WaitingPage = ({ params }) => {
@@ -14,13 +13,7 @@ const WaitingPage = ({ params }) => {
       <div className="absolute bottom-[284px] flex flex-col items-center justify-center gap-4 pt-6 md:static md:h-auto md:min-h-0 md:justify-center md:pt-0">
         <PlayerGrid />
         <GameIdCopyButton gameId={gameId} />
-        <Button
-          onClick={() => {
-            console.log('게임 시작')
-          }}
-        >
-          게임 시작
-        </Button>
+        <LinkButton href={`/game/${gameId}`}>게임 시작</LinkButton>
       </div>
 
       <ChatContainer />
@@ -29,7 +22,3 @@ const WaitingPage = ({ params }) => {
 }
 
 export default WaitingPage
-
-{
-  /* <LinkButton href={`/game/${gameId}`}>게임 시작</LinkButton> */
-}
