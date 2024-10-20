e3e598d1ce5ba1f870433b3e7b480fbbcfe98a67
seoyoonyi <yiseoyoon@gmail.com>
2024-10-20 19:34:04 +0900

    feat: 게임 ID 입력 및 상태 관리 추가, 페이지 링크 수정 (#42)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from 'react'
+
 import TrendingFlatIcon from '@mui/icons-material/TrendingFlat'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -7,32 +11,34 @@ import Input from '@/components/ui/input'
 import LinkButton from '@/components/ui/link-button'
 import logo from '@/public/images/logo.png'
 
-const HomePage = ({ params }) => {
-  const { gameId = 'N09C14' } = params
+const HomePage = () => {
+  const [gameId, setGameId] = useState('')
+
+  const handleGameIdChange = (e) => {
+    setGameId(e.target.value)
+  }
 
   return (
-    <>
-      <main className="flex min-h-dvh flex-col justify-center bg-sky-mobile bg-cover bg-fixed bg-center md:bg-sky-desktop">
-        <div className="mb-20 flex justify-center">
-          <Image src={logo} alt="로고" width={280} height={140} />
+    <main className="flex min-h-dvh flex-col justify-center bg-sky-mobile bg-cover bg-fixed bg-center md:bg-sky-desktop">
+      <div className="mb-20 flex justify-center">
+        <Image src={logo} alt="로고" width={280} height={140} />
+      </div>
+
+      <div className="flex flex-col items-center gap-4">
+        <LinkButton href="/setup/select-rounds">방 만들기</LinkButton>
+
+        <div className="relative">
+          <Input value={gameId} onChange={handleGameIdChange} placeholder="N09C14" />
+          <Link href={`/setup/user-info`}>
+            <TrendingFlatIcon className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer text-gray-300" />
+          </Link>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <LinkButton href="/setup/select-days">방 만들기</LinkButton>
+        {/* 오류메세지 */}
 
-          <div className="relative">
-            <Input value={''} />
-            <Link href={`/setup/user-info/${gameId}`} passHref>
-              <TrendingFlatIcon className="absolute right-3 top-1/2 -translate-y-1/2 transform cursor-pointer text-gray-300" />
-            </Link>
-          </div>
-
-          {/* 오류메세지 */}
-
-          <GuideButton></GuideButton>
-        </div>
-      </main>
-    </>
+        <GuideButton></GuideButton>
+      </div>
+    </main>
   )
 }
 
