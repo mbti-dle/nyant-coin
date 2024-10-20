b221e7f3eeb02f18a52c144ffd1265b605268df5
seoyoonyi <yiseoyoon@gmail.com>
2024-10-20 19:53:45 +0900

    feat: SelectDaysPage 뒤로가기 버튼 추가 및 경로 수정 (#42)

--- app/setup/select-rounds/page.tsx
+++ app/setup/select-rounds/page.tsx
@@ -2,12 +2,14 @@
 
 import { useState } from 'react'
 
+import { ArrowBackIosNew } from '@mui/icons-material'
+import Link from 'next/link'
+
 import LinkButton from '@/components/ui/link-button'
 import RadioGroup, { RadioButton } from '@/components/ui/radio'
 import { DAY_OPTIONS } from '@/constants/game'
 
 const SelectDaysPage = () => {
-  const gameId = 'N09C14'
   const [selectedDay, setSelectedDay] = useState(DAY_OPTIONS[0])
 
   const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,19 +18,22 @@ const SelectDaysPage = () => {
 
   return (
     <main className="flex min-h-dvh flex-col items-center justify-center">
+      <Link href="/" className="absolute left-0 top-0 mx-4 mt-6">
+        <ArrowBackIosNew />
+      </Link>
       <h1 className="px-3 text-center text-xl">시장에서 거래할 일수를 정해주세요.</h1>
       <RadioGroup className="my-20">
         {DAY_OPTIONS.map((day) => (
           <RadioButton
             key={day}
-            name="select-days"
+            name="select-rounds"
             label={day}
             isChecked={day === selectedDay}
             onChangeInput={handleChangeInput}
           />
         ))}
       </RadioGroup>
-      <LinkButton href={`/setup/user-info/${gameId}`}>다음</LinkButton>
+      <LinkButton href={`/setup/user-info`}>다음</LinkButton>
     </main>
   )
 }
