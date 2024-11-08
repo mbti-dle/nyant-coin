5aad98b24fa01909f0c977ed43a4928ed77970b6
nanafromjeju <nanafromjeju@gmail.com>
2024-11-09 03:34:23 +0900

    bug: 인게임 마진탑 수정 (#99)

--- app/game/[gameId]/page.tsx
+++ app/game/[gameId]/page.tsx
@@ -170,7 +170,7 @@ const GamePage = ({ params }) => {
   const totalCoin = gameState.fish * lastFishCoin + gameState.coins
 
   return (
-    <main className="relative h-screen min-h-screen w-full flex-col bg-ocean-game-mobile bg-cover bg-fixed p-3 md:bg-ocean-game-desktop">
+    <main className="relative h-screen min-h-screen w-full flex-col bg-ocean-game-mobile bg-cover bg-fixed p-3 pt-[0px] md:bg-ocean-game-desktop">
       <div className="mx-auto max-w-[420px] flex-col items-center justify-center p-3 md:pt-[50px]">
         <div className="my-6 flex justify-between">
           <div className="flex justify-start">
