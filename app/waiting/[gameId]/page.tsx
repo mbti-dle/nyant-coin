7c1bbb3542472cdd215f6cc5d73e813270ce338c
nanafromjeju <nanafromjeju@gmail.com>
2024-11-09 03:31:15 +0900

    bug: 레이아웃 수정 (#99)

--- app/waiting/[gameId]/page.tsx
+++ app/waiting/[gameId]/page.tsx
@@ -72,7 +72,7 @@ const WaitingPage = ({ params }) => {
     }, 2000)
   }
   return (
-    <main className="relative mx-auto min-h-screen w-full bg-ocean-game-mobile bg-sea-spaceship-mobile bg-cover bg-fixed bg-top p-3 pt-[30px] md:bg-sea-spaceship-desktop">
+    <main className="relative mx-auto min-h-screen w-full bg-ocean-game-mobile bg-sea-spaceship-mobile bg-cover bg-fixed bg-top p-3 pt-[10px] md:bg-sea-spaceship-desktop">
       <div className="mx-auto mt-5 max-w-[420px] flex-col items-center justify-center gap-4 p-3 pb-44 md:pt-[50px]">
         <PlayerGrid players={players} />
         <div className="mt-5 flex flex-col items-center justify-center gap-4">
