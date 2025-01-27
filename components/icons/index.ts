e93c006c238cfbfb3ca81eca7849b1bd75dd7ce8
seoyoonyi <yiseoyoon@gmail.com>
2025-01-27 17:34:59 +0900

    feat: 아이콘 중앙 관리 디렉토리 및 export 파일 추가 (#130)

---
+++ components/icons/index.ts
@@ -0,0 +1,25 @@
+import {
+  MdArrowBackIosNew,
+  MdOutlineTrendingFlat,
+  MdExpandMore,
+  MdExpandLess,
+  MdSend,
+  MdError,
+  MdContentCopy,
+  MdArrowLeft,
+  MdArrowRight,
+  MdClose,
+  MdCheckCircle,
+} from 'react-icons/md'
+
+export const TrendingFlatIcon = MdOutlineTrendingFlat
+export const ArrowBackIcon = MdArrowBackIosNew
+export const ExpandMoreIcon = MdExpandMore
+export const ExpandLessIcon = MdExpandLess
+export const SendIcon = MdSend
+export const ErrorIcon = MdError
+export const ContentCopyIcon = MdContentCopy
+export const ArrowLeftIcon = MdArrowLeft
+export const ArrowRightIcon = MdArrowRight
+export const CloseIcon = MdClose
+export const CheckCircleIcon = MdCheckCircle
